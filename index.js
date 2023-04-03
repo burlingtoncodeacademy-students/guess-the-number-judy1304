88b3477f621dded387871faad6d24dfc6d4cf574
Judith Tuyisingize <judithtuyisingize@Judiths-MacBook-Pro.local>
2023-04-03 03:28:38 -0400

    added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { randomInt } = require('crypto');
+const { exit } = require('process');
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -10,9 +12,47 @@ function ask(questionText) {
 start();
 
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
+  let userInput = await ask("Ready to play a guess game? ");
+  // Check to see if the user typed in Y
+  console.log(userInput)
+
+  if (!userInput.toLowerCase() == "y") {
+    // THIS WILL END THE GAME
+    return process.exit();
+
+  }
+  // Random numbers to choose. Also added tracker for how many guesses. 
+
+  let guesses = 0
+  let min = 1;
+  let max = 100;
+  let guess = Math.floor((max - min) / 2 + min);
+
+  // The while loop if the user guessed it correct or no. 
+  while (true) {
+    guess = Math.floor((max - min) / 2 + min);
+    console.log(`Is it... ${guess}?`);
+    userInput = await ask("yes or no")
+    if (userInput.toLowerCase() === "yes") {
+      console.log(`Your number was ${guess}! you guessed it ${guesses} tries!`);
+      return process.exit();
+    } else if (userInput.toLowerCase() === "no")
+
+
+      // The while loop for computer to ask questions. User input the response 
+      computerResponse = await ask("Is it higher (H), or lower (L)?");
+    console.log(computerResponse);
+    if (computerResponse.toLowerCase() === "h") {
+      min = guess + 1
+      guesses++
+    } else if (computerResponse.toLowerCase() === "l") {
+      max = guess - 1
+      guesses++
+    } else {
+      console.log("Invalid input. Please enter, 'h', 'l', or 'y'. ");
+
+    }
+
+  }
 }
+
